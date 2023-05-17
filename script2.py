197b395ae36027ce530619570475f5bc29a04d2d
Augh <jonathan.hughes@flemingcollege.ca>
2023-05-17 16:01:28 -0400

    Put comment back in script2.py
    Comment was unintentionally deleted by revert.

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+#This is good programming advice
 print("There should be one-- and preferably only one --obious wat to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
