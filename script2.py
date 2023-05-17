498cab903e999c929d750fe434930be1e5d6978a
Augh <jonathan.hughes@flemingcollege.ca>
2023-05-17 16:10:10 -0400

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #This is good programming advice
 print("There should be one-- and preferably only one --obious wat to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("Namespace are one honking great idea -- let's do more of those!")
