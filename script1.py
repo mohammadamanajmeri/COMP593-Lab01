2272f35a7dc0f66b67fb7e2930e9f577d56ad34b
Augh <jonathan.hughes@flemingcollege.ca>
2023-05-17 15:58:28 -0400

    Revert "Modified both script files"
    This reverts commit 44849cf30cb30d3451600b880f25c231b9ea3c78.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 # print lines from Zen of Python
 print("There should be one-- and preferably only one --obious wat to do it.")
+print("Although that way may not be obvious at first unless you're Dutch.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# print lines from Zen of Python
 print("There should be one-- and preferably only one --obious wat to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
